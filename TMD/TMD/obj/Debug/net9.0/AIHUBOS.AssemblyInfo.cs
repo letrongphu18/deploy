b17b17c501a798f639804976c35cee090eec1880
letrongphu18 <trphu1810@gmail.com>
2025-11-30 19:03:12 +0700

    ssssasd

--- TMD/TMD/obj/Debug/net9.0/AIHUBOS.AssemblyInfo.cs
+++ TMD/TMD/obj/Debug/net9.0/AIHUBOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHUBOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0c1475034bcbd1340898b80c98f2ae61fc4492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9cba526bfcf3b133069a0d20582126ad958ebb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHUBOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHUBOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
